a5953173b07ae0cce3ac91e2f2a746c2d771043d
Sreeram S <40134790+sreeram2001@users.noreply.github.com>
2021-07-09 00:17:23 +0530

    Add files via upload
    finding maximum element count using hashmaps

---
+++ max_count_hashmap.py
@@ -0,0 +1,26 @@
+
+#finding max element using hashmap
+
+a = [12,14,16,12,14,16,12,17]
+dict = {}
+l = len(a)
+count_max = 0
+maxvalue_index = 0
+
+
+for i in range(l):
+    if a[i] in dict:
+        dict[a[i]] = dict[a[i]] + 1         #initialse the count by 1 for the element
+    else:
+        dict[a[i]] = 1              #initialse count for the num
+        
+
+
+for k in dict:
+    maxi = dict[k]                  #gets frequency of the element
+    if maxi>= count_max:
+        count_max = maxi            #updates the max frequency
+        maxvalue_index = k          #element with max counts
+        
+print(maxvalue_index)
+
